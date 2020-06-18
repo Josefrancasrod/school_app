b371f3e80fcb5218bf903bec58204c264a59359f
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2020-06-17 23:03:34 -0500

    First Commit

---
+++ lib/screens/homework_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeworkScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('My Homeworks'),
+        ),
+      ),
+      body: Center(
+        child: Text('Homework screen'),
+      ),
+    );
+  }
+}
